98e83a44dc25ea538188f54846b3ea786251a352
Graham Hadgraft <graham.hadgraft@gmail.com>
2018-02-18 19:48:36 +0000

    Handle emails better to covert into names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def get_name_from(email)
-    email.gsub!(/\@.*/, '')
-    email.humanize
+    email.gsub!(/\@.*/, '').gsub(/[\-|\_|\.]/, ' ')
+    email.capitalize
   end
 end
